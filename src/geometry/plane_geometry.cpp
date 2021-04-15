bd811a55518676891c5311f641ec9daca21652fa
Adam Söderström <adamsdm1@gmail.com>
2021-04-15 22:12:06 +0200

    Change plane buffers to vectors

--- src/geometry/plane_geometry.cpp
+++ src/geometry/plane_geometry.cpp
@@ -12,13 +12,13 @@ namespace GE{
       float half_w = width/2.0f;
       float half_h = height/2.0f;
       
-      float vertices[] = {
-           half_w,  half_h, 0.0f,  // top right
-           half_w, -half_h, 0.0f,  // bottom right
-          -half_w, -half_h, 0.0f,  // bottom left
-          -half_w,  half_h, 0.0f   // top left 
+      Vertices = {
+          Vertex{ glm::vec3( half_w,  half_h, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f)},  
+          Vertex{ glm::vec3( half_w, -half_h, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.0f)},  
+          Vertex{ glm::vec3(-half_w, -half_h, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)},  
+          Vertex{ glm::vec3(-half_w,  half_h, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f)},
       };
-      unsigned int indices[] = {
+      Indices = {
           0, 1, 3,  // first Triangle
           1, 2, 3   // second Triangle
       };
@@ -30,13 +30,22 @@ namespace GE{
       glBindVertexArray(VAO);
 
       glBindBuffer(GL_ARRAY_BUFFER, VBO);
-      glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+      glBufferData(GL_ARRAY_BUFFER, Vertices.size() * sizeof(Vertex), &Vertices[0], GL_STATIC_DRAW);
 
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-      glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+      glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.size() * sizeof(unsigned int), &Indices[0], GL_STATIC_DRAW);
 
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+      // position attribute
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) 0 );
       glEnableVertexAttribArray(0);
+
+      // texture coord attribute
+      glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) offsetof(Vertex, Normal) );
+      glEnableVertexAttribArray(1);
+
+      // normal attribute
+      glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) offsetof(Vertex, TexCoords) );
+      glEnableVertexAttribArray(2);
 
       glBindBuffer(GL_ARRAY_BUFFER, 0); 
       glBindVertexArray(0); 
@@ -57,7 +66,7 @@ namespace GE{
 
     void PlaneGeometry::Render() const {
       glBindVertexArray(VAO);
-      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
     }
   } 
 }
